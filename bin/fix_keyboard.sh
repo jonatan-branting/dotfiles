1b55fb27552f92609300bd2255207bb962ce6f62
Jonatan Branting <jonatan.branting@gmail.com>
2015-12-20 15:24:25 +0100

    added xmodmap

--- bin/fix_keyboard.sh
+++ bin/fix_keyboard.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-setxkbmap -option caps:escape
 xset r rate 450 21
 xset -b
+
+# Make caps:escape + ctrl!
+setxkbmap -option "ctrl:nocaps"
+xcape -e 'Control_L=Escape' -t 300
+
+# Apply everything? Not sure what this is.
 xmodmap ~/.Xmodmap
