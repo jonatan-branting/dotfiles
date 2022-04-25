9f862505d222306e83a4f96c3abade019192fe1a
Jonatan Branting <jonatan.branting@gmail.com>
2022-04-25 23:00:31 +0200

    Fix deprecation in treesitter module

--- config/nvim/lua/modules/treesitter.lua
+++ config/nvim/lua/modules/treesitter.lua
@@ -1,7 +1,7 @@
 local treesitter = require("nvim-treesitter.configs")
 
 treesitter.setup {
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   textsubjects = {
     enable = true,
     keymaps = {
