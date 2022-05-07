33b7ac743e3b89785c9fb0d516d1e331b10e2ffd
Jonatan Branting <jonatan.branting@gmail.com>
2022-05-07 20:05:07 +0200

    Add `autocmd` module, copied from `nvim-cmp`

---
+++ config/nvim/lua/modules/autocmd.lua
@@ -0,0 +1,31 @@
+local autocmd = {}
+
+autocmd.events = {}
+
+---Subscribe autocmd
+---@param event string
+---@param callback function
+---@return function
+autocmd.subscribe = function(event, callback)
+  autocmd.events[event] = autocmd.events[event] or {}
+  table.insert(autocmd.events[event], callback)
+  return function()
+    for i, callback_ in ipairs(autocmd.events[event]) do
+      if callback_ == callback then
+        table.remove(autocmd.events[event], i)
+        break
+      end
+    end
+  end
+end
+
+---Emit autocmd
+---@param event string
+autocmd.emit = function(event, ...)
+  autocmd.events[event] = autocmd.events[event] or {}
+  for _, callback in ipairs(autocmd.events[event]) do
+    callback(...)
+  end
+end
+
+return autocmd
