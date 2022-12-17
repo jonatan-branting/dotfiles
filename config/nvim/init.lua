7b2b1de078e33134fe657931dbb45f3fb4918168
Jonatan Branting <jonatan.branting@gmail.com>
2022-12-17 23:00:14 +0100

    Add highlight to yanked text

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -718,3 +718,13 @@ vim.api.nvim_create_autocmd("BufWritePost",
     end
   }
 )
+
+vim.api.nvim_create_autocmd("TextYankPost",
+  {
+    pattern = "*",
+    group = group,
+    callback = function(args)
+      vim.highlight.on_yank { higroup = "IncSearch", timeout = 400 }
+    end
+  }
+)
