e616a8cd20ea4dc2ca0b2deb956c862ccd5e99c4
Jonatan Branting <jonatan.branting@gmail.com>
2022-12-18 21:07:03 +0100

    Fix glance setup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -50,15 +50,13 @@ require("packer").startup({
             },
           },
           hooks = {
-            hooks = {
-              before_open = function(results, open, jump, method)
-                if #results == 1 then
-                  jump(results[1]) -- argument is optional
-                else
-                  open(results) -- argument is optional
-                end
-              end,
-            }
+            before_open = function(results, open, jump, method)
+              if #results == 1 then
+                jump(results[1]) -- argument is optional
+              else
+                open(results) -- argument is optional
+              end
+            end,
           }
         })
 
