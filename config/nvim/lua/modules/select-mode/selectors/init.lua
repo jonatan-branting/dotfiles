8b5ae7e525d1473626eb4f84a8d208fe6c17a069
Jonatan Branting <jonatan.branting@gmail.com>
2023-01-04 09:47:24 +0100

    chore: remove unused select-mode code

--- config/nvim/lua/modules/select-mode/selectors/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-  range = require("modules.select-mode.selectors.range_select"),
-  word = require("modules.select-mode.selectors.word_select"),
-  char = require("modules.select-mode.selectors.char_select"),
-}
